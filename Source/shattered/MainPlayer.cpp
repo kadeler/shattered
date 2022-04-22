43bc2446417575ad1b8dbf37a6ba6b02cc9a4319
Evan <mensyoo2370@gmail.com>
2022-04-23 02:33:04 +0300

    additional commit

---
+++ Source/shattered/MainPlayer.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AMainPlayer::AMainPlayer() : BaseLookXRate(45.f), BaseLookYRate(45.f)
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 0.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+// Called when the game starts or when spawned
+void AMainPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMainPlayer::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0,Rotation.Yaw,0 };
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::X) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMainPlayer::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0,Rotation.Yaw,0 };
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::Y) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
+// Called every frame
+void AMainPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+}
+
+// Called to bind functionality to input
+void AMainPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainPlayer::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainPlayer::MoveRight);
+	PlayerInputComponent->BindAxis("LookX", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookY", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+}
+
